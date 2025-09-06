9edef42f2d39e1f0faf22fbbfb742c148830cb22
Filipe Johansson <filipejohansson7@gmail.com>
2025-09-06 19:09:44 -0300

    change to use gosocket package

--- go.mod
+++ go.mod
@@ -1,5 +1,7 @@
 module github.com/filipejohansson/go-echo-server
 
-go 1.24.2
+go 1.24.3
 
-require github.com/gorilla/websocket v1.5.3
+require github.com/gorilla/websocket v1.5.3 // indirect
+
+require github.com/FilipeJohansson/gosocket v0.1.0
--- main.go
+++ main.go
@@ -4,40 +4,26 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/websocket"
+	"github.com/FilipeJohansson/gosocket"
 )
 
-var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool { return true },
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println("Error doing upgrade:", err)
-		return
-	}
-	defer conn.Close()
-
-	log.Println("Client connected")
-	for {
-		messageType, message, err := conn.ReadMessage()
-		if err!= nil {
-			log.Println("Error reading message:", err)
-			break
-		}
-		log.Println("Message received:", string(message))
-
-		if err := conn.WriteMessage(messageType, message); err != nil {
-			log.Println("Error to write message:", err)
-			break
-		}
-	}
-	log.Println("Client disconnected")
-}
-
 func main() {
-	http.HandleFunc("/ws", handler)
+	handler := gosocket.NewHandler()
+	handler.
+		OnConnect(func(c *gosocket.Client) error {
+			log.Println("Client connected")
+			return nil
+		}).
+		OnMessage(func(c *gosocket.Client, m *gosocket.Message) error {
+			log.Println("Message received:", string(m.RawData))
+			c.SendMessage(m)
+			return nil
+		}).OnDisconnect(func(c *gosocket.Client) error {
+		log.Println("Client disconnected")
+		return nil
+	})
+
+	http.Handle("/ws", handler)
 	log.Println("Server started at :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
